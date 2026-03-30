d0b8fd3c5969c7ce0a702aa8bb692485ff737e92
Eunji Lee <KHenzhi1027@gmail.com>
2026-03-30 17:31:26 +0900

    Initial commit

---
+++ semi_project_back/src/main/java/kr/co/iei/SemiProjectBackApplication.java
@@ -0,0 +1,13 @@
+package kr.co.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectBackApplication.class, args);
+	}
+
+}
