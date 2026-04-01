72769d661c355f2020d8f7f96c21011adab86570
biblosjm <biblosjm@naver.com>
2026-04-01 17:12:18 +0900

    2026-04-01 5:11
    코스목록, 코스상세보기

--- semi_project_front/src/App.jsx
+++ semi_project_front/src/App.jsx
@@ -8,6 +8,7 @@ import CourseListPage from "./pages/course/CourseListPage";
 import AttractionSearchPage from "./pages/attraction/AttractionSearchPage";
 import { useState } from "react";
 import Login from "./pages/attraction/member/Login";
+import CourseViewPage from "./pages/course/CourseViewPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/course/list" element={<CourseListPage />} />
+            <Route path="/course/view" element={<CourseViewPage />} />
             <Route path="/attraction/list" element={<AttractionSearchPage />} />
             <Route path="/member/login" element={<Login />} />
           </Routes>
